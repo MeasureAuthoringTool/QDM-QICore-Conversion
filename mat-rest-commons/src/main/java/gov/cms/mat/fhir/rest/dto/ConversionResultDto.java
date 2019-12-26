5d45a77754b31140337a14003989d615a5065e0a
Michael C Greene <greenemcg@gmail.com>
2019-12-26 11:02:36 -0700

    MAT-141 added lombok default constructor

--- mat-rest-commons/src/main/java/gov/cms/mat/fhir/rest/dto/ConversionResultDto.java
+++ mat-rest-commons/src/main/java/gov/cms/mat/fhir/rest/dto/ConversionResultDto.java
@@ -2,11 +2,13 @@ package gov.cms.mat.fhir.rest.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Builder
 @Getter
+@NoArgsConstructor
 public class ConversionResultDto {
     private String measureId;
     private Instant modified;
