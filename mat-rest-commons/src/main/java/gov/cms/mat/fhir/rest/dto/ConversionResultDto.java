b995513ba6cead1d616ca2fb5fabe76dfdc91744
Michael C Greene <greenemcg@gmail.com>
2019-12-26 11:03:30 -0700

    MAT-141 added lombok getters

--- mat-rest-commons/src/main/java/gov/cms/mat/fhir/rest/dto/ConversionResultDto.java
+++ mat-rest-commons/src/main/java/gov/cms/mat/fhir/rest/dto/ConversionResultDto.java
@@ -3,11 +3,13 @@ package gov.cms.mat.fhir.rest.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 public class ConversionResultDto {
     private String measureId;
