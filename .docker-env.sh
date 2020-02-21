c3c84ef02f2d33e7f3557ffe538dab89d05443e4
carson.day <carson.day@semanticbits.com>
2020-02-21 17:04:20 -0500

    Changed to jdk11. - Fixed an issue in FhirCqlLibraryFileHandler.java loading files from resources in java11. - Java11 changes with dependencies. - Reorganized the pom.xml structure and the parent project.

---
+++ .docker-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export VSAC_USER=ddecouteau24
+export VSAC_PASS=R1v3r0n3!#
+export MAT_DB=mat
+export MAT_DB_USER=root
+export MAT_DB_PASS=password
+export MYSQL_ROOT_PASSWORD=password
---
+++ create-links.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo mkdir -p /opt/library
+sudo mkdir -p /opt/vsac
+sudo mkdir -p /opt/library/uncoverted/
+sudo ln -f -s mat-fhir-services/fhir/  /opt/library/fhir
+sudo ln -f -s mat-fhir-services/cache/ /opt/vsac/cache
