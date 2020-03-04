75dc238de7b8d638e27120a3898231ee167d2f66
carson.day <carson.day@semanticbits.com>
2020-03-04 09:12:51 -0500

    Used an absolute path when creating links. Relative path was causing issues in test cases.

--- create-links.sh
+++ create-links.sh
@@ -2,5 +2,5 @@
 sudo mkdir -p /opt/library
 sudo mkdir -p /opt/vsac
 sudo mkdir -p /opt/library/uncoverted/
-sudo ln -f -s mat-fhir-services/fhir/  /opt/library/fhir
-sudo ln -f -s mat-fhir-services/cache/ /opt/vsac/cache
+sudo ln -f -s `pwd`/mat-fhir-services/fhir/  /opt/library/fhir
+sudo ln -f -s `pwd`/mat-fhir-services/cache/ /opt/vsac/cache
