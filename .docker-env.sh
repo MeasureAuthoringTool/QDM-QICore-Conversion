8bab89fe590dcc5e98c9e703d428a0903cb3ede9
carson.day <carson.day@semanticbits.com>
2020-03-05 15:04:24 -0500

    Added docker-env.sh to .getignore

--- .docker-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export VSAC_USER=***REMOVED***
-export VSAC_PASS=***REMOVED***
-export MAT_DB=mat
-export MAT_DB_USER=root
-export MAT_DB_PASS=password
-export MYSQL_ROOT_PASSWORD=password
