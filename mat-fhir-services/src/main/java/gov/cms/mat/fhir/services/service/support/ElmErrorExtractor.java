aa15e90fd31670d66134f2e01338ba58348aa550
Michael C Greene <greenemcg@gmail.com>
2020-04-07 12:56:59 -0600

    Code clean up

--- mat-fhir-services/src/main/java/gov/cms/mat/fhir/services/service/support/ElmErrorExtractor.java
+++ mat-fhir-services/src/main/java/gov/cms/mat/fhir/services/service/support/ElmErrorExtractor.java
@@ -81,7 +81,6 @@ public class ElmErrorExtractor {
                 .build();
     }
 
-
     public List<MatCqlConversionException> parseForErrorExceptions() {
         try {
             JsonNode errorExceptionsNode = getErrorExceptionsNode();
