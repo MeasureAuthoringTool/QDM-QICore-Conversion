14fad3d1b5892fecdd155a1d8f5b554820efcb1d
carson.day <carson.day@semanticbits.com>
2020-05-19 09:30:08 -0400

    Added updateHapiGlobalCommonLibs.sh script.

---
+++ scripts/updateHapiGlobalCommonLibs.sh
@@ -0,0 +1,27 @@
+# FHIR_SERVICES_URL='http://localhost:9080'
+FHIR_SERVICES_URL=https://matdev.semanticbits.com/mat-fhir-services
+COMMON_LIBS=('FHIRHelpers-4-0-001' 'MATGlobalCommonFunctions-FHIR4-5-0-000' 'SupplementalDataElements-FHIR4-2-0-000' 'AdultOutpatientEncounters-FHIR4-2-0-000' 'AdvancedIllnessandFrailtyExclusion-FHIR4-5-0-000' 'AdvancedIllnessandFrailtyExclusion-FHIR4-5-0-000' 'Hospice-FHIR4-2-0-000' 'TJCOverall-FHIR4-5-0-000' 'VTEICU-FHIR4-4-0-000')
+
+echo "Deleting all libs"
+curl --location --request DELETE $FHIR_SERVICES_URL'/library/deleteAll'
+
+echo""
+echo""
+echo "Deleting all measures"
+curl --location --request DELETE $FHIR_SERVICES_URL"/measure/deleteAll"
+
+echo""
+echo""
+echo "Pushing all FHIR Global Common Libs..."
+for l in ${COMMON_LIBS[@]}; do
+  echo ""
+  echo "Pushing $l"
+  curl --location --request POST $FHIR_SERVICES_URL"/library/pushStandAloneLibrary?id=$l"
+done
+echo""
+echo "Complete"
+
+
+
+
+
