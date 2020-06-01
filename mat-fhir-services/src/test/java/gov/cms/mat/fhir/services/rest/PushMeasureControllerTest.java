d34f7d401cf734008d72b0a8074e052ff17935ca
michael.greene <greenemcg@gmail.com>
2020-06-01 09:00:29 -0600

    Test Coverage

---
+++ mat-fhir-services/src/test/java/gov/cms/mat/fhir/services/rest/PushMeasureControllerTest.java
@@ -0,0 +1,36 @@
+package gov.cms.mat.fhir.services.rest;
+
+import gov.cms.mat.fhir.services.components.mongo.ConversionResultsService;
+import gov.cms.mat.fhir.services.service.orchestration.PushMeasureService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PushMeasureControllerTest {
+    private final static String ID = "id";
+    private final static  String URL = "http://hapi.com/Measure/2";
+    @Mock
+    private PushMeasureService pushMeasureService;
+    @Mock
+    private ConversionResultsService conversionResultsService;
+
+    @InjectMocks
+    private PushMeasureController pushMeasureController;
+
+    @Test
+    void convertStandAloneFromMatToFhir() {
+        when(pushMeasureService.convert(anyString(), any())).thenReturn(URL);
+
+        String result = pushMeasureController.pushMeasure(ID, "batchId");
+
+        assertEquals(URL, result);
+    }
+}
