e20e4976bd4142af9f4b134b41e02c6db73ebfa6
michael.greene <greenemcg@gmail.com>
2020-06-12 08:00:37 -0600

    added mising file

---
+++ mat-fhir-services/src/main/java/gov/cms/mat/fhir/services/config/MatFhirConfiguration.java
@@ -0,0 +1,37 @@
+package gov.cms.mat.fhir.services.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties("mat-fhir") // prefix app, find app.* values
+public class MatFhirConfiguration {
+
+    private List<ThreadPoolConfigurations> threadPoolConfigurations = new ArrayList<>();
+
+    @PostConstruct
+    public void showMe() {
+        System.out.println("here");
+    }
+
+    public List<ThreadPoolConfigurations> getThreadPoolConfigurations() {
+        return threadPoolConfigurations;
+    }
+
+    public void setThreadPoolConfigurations(List<ThreadPoolConfigurations> threadPoolConfigurations) {
+        this.threadPoolConfigurations = threadPoolConfigurations;
+    }
+
+    @Data
+    static class ThreadPoolConfigurations {
+        private String name;
+        private Integer corePoolSize;
+        private Integer maxPoolSize;
+        private Integer queueCapacity;
+    }
+}
