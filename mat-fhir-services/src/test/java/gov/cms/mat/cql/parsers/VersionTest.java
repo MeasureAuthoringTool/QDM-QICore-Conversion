87ffae4a541202de95abcccfe6b0c7de102a5695
carson.day <carson.day@semanticbits.com>
2020-06-24 12:22:42 -0400

    Codacy fixes

--- mat-fhir-services/src/test/java/gov/cms/mat/cql/parsers/VersionTest.java
+++ mat-fhir-services/src/test/java/gov/cms/mat/cql/parsers/VersionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class VersionTest {
@@ -18,14 +19,14 @@ public class VersionTest {
         assertTrue(p.matcher("999.999.000").matches());
         assertTrue(p.matcher("999.999.999").matches());
 
-        assertTrue(!p.matcher("0.0").matches());
-        assertTrue(!p.matcher("0").matches());
-        assertTrue(!p.matcher("0.0.0").matches());
-        assertTrue(!p.matcher("0.0.00").matches());
+        assertFalse(p.matcher("0.0").matches());
+        assertFalse(p.matcher("0").matches());
+        assertFalse(p.matcher("0.0.0").matches());
+        assertFalse(p.matcher("0.0.00").matches());
 
-        assertTrue(!p.matcher("9.9").matches());
-        assertTrue(!p.matcher("9").matches());
-        assertTrue(!p.matcher("9.9.9").matches());
-        assertTrue(!p.matcher("9.9.99").matches());
+        assertFalse(p.matcher("9.9").matches());
+        assertFalse(p.matcher("9").matches());
+        assertFalse(p.matcher("9.9.9").matches());
+        assertFalse(p.matcher("9.9.99").matches());
     }
 }
