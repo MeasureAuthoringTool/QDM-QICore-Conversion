5d1f9a2408539f66871955e318a66ce97c5a1b40
carson.day <carson.day@semanticbits.com>
2020-07-15 15:13:04 -0400

    MAT-1335 Removed some changes that shouldn't be checked in.

--- mat-rest-commons/src/main/test/java/gov/cms/mat/cql/parsers/DefineParserTest.java
+++ mat-rest-commons/src/main/test/java/gov/cms/mat/cql/parsers/DefineParserTest.java
@@ -17,26 +17,4 @@ class DefineParserTest {
                 "  \t\t)";
 
     }
-
-
-    define "Cessation Programs":
-            ( ["Encounter, Performed": "Tobacco User with Cessation Intervention"]
-              union ["Procedure, Performed": "Tobacco User with Cessation Intervention"]
-              union ["Encounter, Performed": "Tobacco Use Cessation Counseling"]
-              union ["Procedure, Performed": "Tobacco Use Cessation Counseling"] ) Counseling
-              with "Most Recent Encounter" ENC
-                such that ( Counseling.relevantPeriod starts 731 days or less after start of ENC.relevantPeriod
-                or Counseling.relevantPeriod starts before start of ENC.relevantPeriod
-            )
 }
-
-    define "Cessation Programs":
-            // Contains UNIONS of different types. Not supported in FHIR.
-            ( ["Encounter, Performed": "Tobacco User with Cessation Intervention"]
-            union ["Procedure, Performed": "Tobacco User with Cessation Intervention"]
-            union ["Encounter, Performed": "Tobacco Use Cessation Counseling"]
-            union ["Procedure, Performed": "Tobacco Use Cessation Counseling"] ) Counseling
-            with "Most Recent Encounter" ENC
-            such that ( Counseling.relevantPeriod starts 731 days or less after start of ENC.relevantPeriod
-            or Counseling.relevantPeriod starts before start of ENC.relevantPeriod
-            )
