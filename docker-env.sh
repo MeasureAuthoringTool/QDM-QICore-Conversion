57f16a4803e8b25a7d80f29604f70e01f061b32d
Carson Day <55844901+cday-semanticbits@users.noreply.github.com>
2020-08-20 13:54:32 -0400

    Update docker-env.sh

--- docker-env.sh
+++ docker-env.sh
@@ -6,10 +6,10 @@ export MAT_DB_URL='jdbc:mysql://host.docker.internal:3306/mat?serverTimezone=UTC
 export MAT_DB_USER=root
 export MAT_DB_PASS=changeme
 
-export HAPI_DB=hapi
+export HAPI_DB=hapi-r4-db
 export HAPI_DB_URL='jdbc:mysql://hapi-fhir-mysql:3306/'${HAPI_DB}'?serverTimezone=UTC&max_allowed_packet=16777216&autoReconnect=true&allowPublicKeyRetrieval=true&useSSL=false'
 export HAPI_DB_USER=admin	
-export HAPI_DB_PASS=admin
+export HAPI_DB_PASS=changeme
 
 export MONGO_DB_URL='mongodb://host.docker.internal:27017'
 
