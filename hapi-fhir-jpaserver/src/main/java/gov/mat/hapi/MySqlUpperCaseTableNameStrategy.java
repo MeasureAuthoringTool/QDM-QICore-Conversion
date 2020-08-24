d617dc318d39cfeb6d2fdc090a4b2671072f4ca8
carson.day <carson.day@semanticbits.com>
2020-08-24 09:39:17 -0400

    BUGFUX Changed to upper case table names to fix hapi db issues.

--- hapi-fhir-jpaserver/src/main/java/gov/mat/hapi/MySqlUpperCaseTableNameStrategy.java
+++ hapi-fhir-jpaserver/src/main/java/gov/mat/hapi/MySqlUpperCaseTableNameStrategy.java
@@ -4,9 +4,9 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
-public class MySqlLowerCaseTableNameStrategy extends PhysicalNamingStrategyStandardImpl {
+public class MySqlUpperCaseTableNameStrategy extends PhysicalNamingStrategyStandardImpl {
     @Override
     public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment context) {
-        return Identifier.toIdentifier(name.getText().toLowerCase());
+        return Identifier.toIdentifier(name.getText().toUpperCase());
     }
 }
