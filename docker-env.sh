b73d2510c416a2863bc344e8aa3f2fa869b473d7
carson.day <carson.day@semanticbits.com>
2020-09-04 14:17:48 -0400

    MAT-1712 Changed default MAT_API_KEY to DISABLED for microservices.

--- docker-env.sh
+++ docker-env.sh
@@ -11,7 +11,7 @@ export HAPI_DB_URL='jdbc:mysql://hapi-fhir-mysql:3306/'${HAPI_DB}'?serverTimezon
 export HAPI_DB_USER=admin	
 export HAPI_DB_PASS=changeme
 
-export MAT_API_KEY=LOCAL_KEY
+export MAT_API_KEY=DISABLED
 
 export HAPI_FHIR_URL_PUBLIC='http://localhost:6060/fhir'
 export HAPI_FHIR_URL='http://hapi-fhir-jpaserver:6060/fhir/'
