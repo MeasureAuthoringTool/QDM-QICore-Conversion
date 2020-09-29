504d9c9853d4b3e3f4621225cf11c78625d5059e
michael.greene <greenemcg@gmail.com>
2020-09-29 13:50:38 -0600

    Return types missing dude to double encoding issue in the reverse

--- mat-fhir-services/src/main/java/gov/cms/mat/fhir/services/components/cql/FhirLibrarySourceProvider.java
+++ mat-fhir-services/src/main/java/gov/cms/mat/fhir/services/components/cql/FhirLibrarySourceProvider.java
@@ -8,28 +8,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
-import java.util.Base64;
 
 @Component
 public class FhirLibrarySourceProvider implements LibrarySourceProvider {
+    private final HapiFhirServer hapiFhirServer;
 
-    @Autowired
-    private HapiFhirServer hapiFhirServer;
+    public FhirLibrarySourceProvider(HapiFhirServer hapiFhirServer) {
+        this.hapiFhirServer = hapiFhirServer;
+    }
 
     @Override
     public InputStream getLibrarySource(VersionedIdentifier versionedIdentifier) {
-
         var lib = hapiFhirServer.fetchHapiLibrary(versionedIdentifier.getId(), versionedIdentifier.getVersion());
         if (lib.isPresent()) {
             byte[] bytes = lib.get().getContent().get(0).getData();
-            String cql = new String(Base64.getDecoder().decode(bytes));
             try {
-                return IOUtils.toInputStream(cql, "utf-8");
+                return IOUtils.toInputStream(new String(bytes), "utf-8");
             } catch (Exception e) {
-                throw new RuntimeException("Could not find lib " + versionedIdentifier.getId()  + "V" + versionedIdentifier.getVersion(), e);
+                throw new RuntimeException("Unknown Error with lib " + versionedIdentifier.getId() + "V" + versionedIdentifier.getVersion(), e);
             }
         } else {
-            throw new RuntimeException("Could not find lib " + versionedIdentifier.getId()  + "V" + versionedIdentifier.getVersion());
+            throw new RuntimeException("Could not find lib " + versionedIdentifier.getId() + "V" + versionedIdentifier.getVersion());
         }
     }
 }
