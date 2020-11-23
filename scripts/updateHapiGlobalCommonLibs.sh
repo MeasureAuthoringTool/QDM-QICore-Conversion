10426114181869133cdac86967224a255d5cbbb4
carson.day <carson.day@semanticbits.com>
2020-11-23 11:51:09 -0500

    Updating cqframework.version to 1.5.1-SNAPSHOT to pull on Bryn's changes.

--- scripts/updateHapiGlobalCommonLibs.sh
+++ scripts/updateHapiGlobalCommonLibs.sh
@@ -16,13 +16,13 @@ FHIR_SERVICES_URL='http://localhost:9080'
 
 COMMON_LIBS=('33b6e38e5e0543ada0e8942b91d42795' '11a37cbfc8634b00a69a56d005f10fd7' '09b6df1d5cfb4ed787d63ffd8e4e32a8' '35e8cdb0bb204ce1a39682093adbf5f1' '1b9d3281e08c4053a24a9f6b911d680e' '747a00aa82d340f29f257d848c125742' '195c13832b09499ca1e1ccc4fb5fd77b' '544333b6ef7e47268672589073a33538')
 
-echo "Deleting all libs"
-curl -H 'MAT-API-KEY: '${MAT_API_KEY} --location --request DELETE $FHIR_SERVICES_URL'/library/deleteAll'
+#echo "Deleting all libs"
+#curl -H 'MAT-API-KEY: '${MAT_API_KEY} --location --request DELETE $FHIR_SERVICES_URL'/library/deleteAll'
 
-echo""
-echo""
-echo "Deleting all measures"
-curl -H 'MAT-API-KEY: '${MAT_API_KEY} --location --request DELETE $FHIR_SERVICES_URL"/measure/deleteAll"
+#echo""
+#echo""
+#echo "Deleting all measures"
+#curl -H 'MAT-API-KEY: '${MAT_API_KEY} --location --request DELETE $FHIR_SERVICES_URL"/measure/deleteAll"
 
 echo""
 echo""
