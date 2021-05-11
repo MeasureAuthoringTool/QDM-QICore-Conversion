f17503ae261ec2f79a68a5758b44e04f5c587e1f
Michael C Greene <greenemcg@gmail.com>
2021-05-11 08:19:36 -0600

    clean up test

--- mat-fhir-services/src/test/java/gov/cms/mat/fhir/services/components/validation/unused/UnusedValidatorTest.java
+++ mat-fhir-services/src/test/java/gov/cms/mat/fhir/services/components/validation/unused/UnusedValidatorTest.java
@@ -4,36 +4,30 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.cms.mat.fhir.services.ResourceFileUtil;
-import gov.cms.mat.fhir.services.cql.parser.CqlToMatXml;
 import gov.cms.mat.fhir.services.cql.parser.CqlVisitorFactory;
 import gov.cms.mat.fhir.services.rest.MatXmlController;
 import gov.cms.mat.fhir.services.rest.dto.UnusedCqlElements;
-import gov.cms.mat.fhir.services.service.UCUMValidationService;
 import mat.model.cql.CQLModel;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class UnusedValidatorTest implements ResourceFileUtil {
-
-    private  CqlVisitorFactory cqlVisitorFactory;
-
     @Test
-    void findVerifyValueSets() throws JsonProcessingException {
+    void findUnusedValueSets() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        String json =  getStringFromResource("/unused/MatCqlXmlReq.json");
-        MatXmlController.MatCqlXmlReq matCqlXmlReq  =   mapper.readValue( json,     MatXmlController.MatCqlXmlReq.class );
+        String json = getStringFromResource("/unused/MatCqlXmlReq.json");
+        MatXmlController.MatCqlXmlReq matCqlXmlReq = mapper.readValue(json, MatXmlController.MatCqlXmlReq.class);
+        assertEquals(2, matCqlXmlReq.getSourceModel().getValueSetList().size());
 
         UnusedValidator unused = new UnusedValidator(matCqlXmlReq.getSourceModel());
-        UnusedCqlElements unusedCqlElements =  unused.findUnused();
+        UnusedCqlElements unusedCqlElements = unused.findUnused();
 
-        assertEquals(unusedCqlElements.getValueSets().size(), 1);
+        assertEquals(1, unusedCqlElements.getValueSets().size());
+        assertEquals("2 Hour Blood Glucose Test", unusedCqlElements.getValueSets().get(0).getCodeListName());
     }
-
 
     @Test
     void findUnused() {
